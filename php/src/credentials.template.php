9beeb464c8e86dccd500df49189729fe5f7c22c9
Yuriy Botsev <YurBoc@yandex.ru>
2025-06-21 08:57:36 +0300

    Add PHP files, config, credentials (template)

---
+++ php/src/credentials.template.php
@@ -0,0 +1,28 @@
+<?php
+
+// +-------------------+
+// |  Параметры FTP    |
+// +-------------------+
+$ftp_server = "your_server_address_here";
+$ftp_user_name = "your_ftp_user_name_here";
+$ftp_user_pass = "your_ftp_user_password_here";
+
+// +------------------------+
+// |  Доступ к Google API   |
+// +------------------------+
+$googleAccountKeyFilePath = __DIR__ . '/FST-OTM-cred.json';
+
+// +--------------------------+
+// |  Доступ к Telegram API   |
+// +--------------------------+
+$telegramBotToken = '000000000:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
+$telegramApiUrl = 'https://api.telegram.org/bot'.$telegramBotToken.'/';
+$telegramChatId = '000000000';
+
+$butovohomeBotToken = '000000000:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
+$butovohomeApiUrl = 'https://api.telegram.org/bot'.$butovohomeBotToken.'/';
+
+$berillosBotToken = '1111111111:BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB';
+$berillosApiUrl = 'https://api.telegram.org/bot'.$berillosBotToken.'/';
+
+?>
